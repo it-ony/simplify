c610b6b022e0c0e071aaaa7a2563965d700e0c4a
Tony Findeisen <github@it-ony.de>
2012-11-05 17:15:37 +0100

    added workaround for fetching collection, because articles not available with /user/shops/articles

---
+++ public/app/module/CollectionClass.js
@@ -0,0 +1,26 @@
+define(['app/module/ModuleBase', "js/data/DataSource", "sprd/model/Shop", "flow"], function(ModuleBase, DataSource, Shop, flow) {
+    return ModuleBase.inherit('app.module.CollectionClass', {
+
+        defaults: {
+            collection: null
+        },
+
+        inject: {
+            api: DataSource
+        },
+
+        showCollection: function(routeContext, collectionId) {
+            var api = this.$.api,
+                collection = api.createEntity(Shop, collectionId);
+
+            this.set("collection", collection);
+
+            flow()
+                .seq(function(cb) {
+                    collection.fetch(null, cb);
+                })
+                .exec(routeContext.callback);
+
+        }.async()
+    });
+});
