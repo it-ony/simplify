9ec45f304dd9ac202aa9fb3e6c0010402e17f21c
Tony Findeisen <github@it-ony.de>
2012-11-05 17:15:03 +0100

    added i18n and user as root injection

--- public/app/module/ModuleBase.js
+++ public/app/module/ModuleBase.js
@@ -1,5 +1,8 @@
-define(['js/core/Module'], function(Module) {
+define(['js/core/Module', 'js/core/I18n'], function(Module, I18n) {
     return Module.inherit({
-
+        injection: {
+            i18n: I18n,
+            user: "user"
+        }
     });
 });
