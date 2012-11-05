44eb9c121e77a68923dde6e6325bf25a31783c90
Tony Findeisen <github@it-ony.de>
2012-11-05 17:34:02 +0100

    implemented start

--- public/app/SimplifyClass.js
+++ public/app/SimplifyClass.js
@@ -1,24 +1,71 @@
-define(
-    ["js/core/Application"],
-    function (Application) {
+define(["js/core/Application", "sprd/model/Session", "flow", "js/data/LocalStorage"], function (Application, Session, flow, LocalStorage) {
 
         return Application.inherit({
-            /**
-             *  initializes the application variables
-             */
-            initialize:function () {
-                this.set('appName','Simplify');
+
+            defaults: {
+                user: null
             },
+
+            inject: {
+                localStorage: LocalStorage
+            },
+
             /***
              * Starts the application
              * @param parameter
              * @param callback
              */
             start:function (parameter, callback) {
-                // false - disables autostart
-                this.callBase(parameter, false);
 
-                callback();
+                var self = this,
+                    api = this.$.api,
+                    localStorage = this.$.localStorage,
+                    injection = this.$.injection,
+                    session;
+
+                flow()
+                    .seq(function(cb) {
+                        // create or load session
+                        var sessionId = localStorage.getItem("sessionId");
+
+                        if (sessionId) {
+                            session = api.createEntity(Session, sessionId);
+                            session.fetch({noCache: true}, function (err) {
+                                if (err) {
+                                    createNewSession();
+                                } else {
+                                    cb();
+                                }
+                            });
+                        } else {
+                            createNewSession();
+                        }
+
+                        function createNewSession() {
+                            session = api.createEntity(Session);
+                            session.login(parameter.username, parameter.password, cb);
+                        }
+                    })
+                    .par(function (cb) {
+                        self.set("user", session.$.user);
+                        session.$.user.$.shops.fetch({
+                            fullData: true
+                        }, cb);
+                    }, function(cb) {
+                        session.$.user.fetch(null, cb);
+                    })
+                    .exec(function(err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            injection.addInstance("user", session.$.user);
+                            localStorage.setItem("sessionId", session.$.id);
+
+                            // call start from super
+                            self.start.baseImplementation.call(self, parameter, callback);
+                        }
+                    });
+
             },
 
             defaultRoute: function (routeContext) {
