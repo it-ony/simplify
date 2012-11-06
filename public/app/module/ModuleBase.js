a68bcd2a95a134325d315538d18964f47506bd99
Tony Findeisen <github@it-ony.de>
2012-11-06 14:25:04 +0100

    fixed injection for ModuleBase

--- public/app/module/ModuleBase.js
+++ public/app/module/ModuleBase.js
@@ -1,6 +1,6 @@
 define(['js/core/Module', 'js/core/I18n'], function(Module, I18n) {
     return Module.inherit({
-        injection: {
+        inject: {
             i18n: I18n,
             user: "user"
         }
