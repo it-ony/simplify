77f94fcc2dd35b9a97bdfe4865931fe4eb39441d
Marcus Krejpowicz <krebbl@gmail.com>
2012-11-28 15:49:37 +0100

    added tile list for designs ...

--- public/app/module/DesignClass.js
+++ public/app/module/DesignClass.js
@@ -1,12 +1,51 @@
-define(['app/module/ModuleBase', "js/data/DataSource", "flow"], function(ModuleBase, DataSource, flow) {
+define(['app/module/ModuleBase', "js/data/DataSource", "flow", 'sprd/model/Shop'], function(ModuleBase, DataSource, flow, Shop) {
     return ModuleBase.inherit('app.module.DesignClass', {
 
         defaults: {
-            collection: null
+            designs: null,
+            searchTerm: "",
+            progress: 0
         },
 
         inject: {
             api: DataSource
+        },
+
+        start: function(){
+
+            var api = this.$.api,
+                shop = api.createEntity(Shop, 205909);
+
+            var self = this;
+
+            flow()
+                .seq(function (cb) {
+                    shop.fetch(null, cb);
+                })
+                .seq(function (cb){
+                    self.set('designs', shop.getCollection('designs'));
+                })
+                .exec();
+
+            this.callBase();
+        },
+
+        designs: function(){
+
+            if(this.$.designs){
+                return this.$.designs.createQueryCollection({
+
+                });
+            }
+
+        }.onChange('designs'),
+
+        _handleDesignSearch: function(e){
+
+        },
+
+        _onSearchKey: function(e){
+
         }
 
     });
