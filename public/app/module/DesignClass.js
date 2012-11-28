154a37dd11106e3299ce5117a586d9671b547774
Tony Findeisen <github@it-ony.de>
2012-11-28 17:36:25 +0100

    show user designs

--- public/app/module/DesignClass.js
+++ public/app/module/DesignClass.js
@@ -2,39 +2,19 @@ define(['app/module/ModuleBase', "js/data/DataSource", "flow", 'sprd/model/Shop'
     return ModuleBase.inherit('app.module.DesignClass', {
 
         defaults: {
-            designs: null,
+            designs: "{user.designs}",
             searchTerm: "",
             progress: 0
         },
 
         inject: {
-            api: DataSource
-        },
-
-        start: function(){
-
-            var api = this.$.api,
-                shop = api.createEntity(Shop, 205909);
-
-            var self = this;
-
-            flow()
-                .seq(function (cb) {
-                    shop.fetch(null, cb);
-                })
-                .seq(function (cb){
-                    self.set('designs', shop.getCollection('designs'));
-                })
-                .exec();
-
-            this.callBase();
+            user: "user"
         },
 
         designs: function(){
 
             if(this.$.designs){
                 return this.$.designs.createQueryCollection({
-
                 });
             }
 
