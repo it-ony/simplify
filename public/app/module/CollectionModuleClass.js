0c486fd178324cd2725f18fe24bc2e0235d948c1
Marcus Krejpowicz <krebbl@gmail.com>
2012-12-04 18:28:51 +0100

    added collections and products

--- public/app/module/CollectionModuleClass.js
+++ public/app/module/CollectionModuleClass.js
@@ -1,13 +1,36 @@
-define(['app/module/ModuleBase', "js/data/DataSource", "sprd/model/Shop", "flow"], function(ModuleBase, DataSource, Shop, flow) {
+define(['app/module/ModuleBase', "js/data/DataSource", "sprd/model/Shop", "flow","js/core/List"], function(ModuleBase, DataSource, Shop, flow, List) {
     return ModuleBase.inherit('app.module.CollectionClass', {
 
         defaults: {
-            collection: null
+            collection: null,
+            showProducts: true,
+            selectedCollection: '{collectionList.selectedItems.at(0)}',
+            selectedArticle: '{articleList.selectedItems.at(0)}'
         },
 
         inject: {
             api: DataSource
         },
+
+        start: function(){
+            // TODO: add name to index data
+            this.$.user.$.shops.fetch({fullData:true});
+
+            this.callBase();
+        },
+
+        _commitSelectedCollection: function(collection){
+            if(collection){
+                this.set('showProducts',false);
+                this.$stage.$history.navigate('m/collections/'+collection.$.id);
+            }
+        },
+
+        defaultRoute: function (routeContext) {
+            this.set('showProducts', true);
+            this.$.collectionList.$.selectedItems.clear();
+            routeContext.callback();
+        }.async(),
 
         showCollection: function(routeContext, collectionId) {
 
@@ -21,7 +44,6 @@ define(['app/module/ModuleBase', "js/data/DataSource", "sprd/model/Shop", "flow"
                     collection.fetch(null, cb);
                 })
                 .exec(routeContext.callback);
-
         }.async()
     });
 });
