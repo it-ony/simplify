5bf0b34a786837cadd8ad01d0a5bcad26c1dd3d3
Tony Findeisen <github@it-ony.de>
2012-12-06 11:14:37 +0100

    updated create module to new version

--- public/app/module/CreateModuleClass.js
+++ public/app/module/CreateModuleClass.js
@@ -2,16 +2,35 @@ define(['app/module/ModuleBase', "js/data/DataSource", "flow"], function(ModuleB
     return ModuleBase.inherit('app.module.CreateClass', {
 
         defaults: {
+            designs: "{user.designs}",
+
             // focused collection
             collection: null,
 
             // the product
-            product: null
+            product: null,
+
+
+            gap: 40,
+            minWidth: 170,
+            maxWidth: 300,
+            pageSize: 40,
+            cols: 2,
+            itemSize: 80
         },
 
         inject: {
             api: DataSource
-        }
+        },
+
+        designs: function () {
+
+            if (this.$.designs) {
+                return this.$.designs.createQueryCollection({
+                });
+            }
+
+        }.onChange('designs')
 
     });
 });
