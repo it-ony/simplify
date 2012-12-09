9b9fdfc85ee1b649d190c7e7e1fe54f5ce7d1d36
Tony Findeisen <github@it-ony.de>
2012-12-09 20:57:55 +0100

    added product viewer ...

--- public/app/module/CreateModuleClass.js
+++ public/app/module/CreateModuleClass.js
@@ -1,12 +1,12 @@
-define(['app/module/ModuleBase', "js/data/DataSource", "flow"], function(ModuleBase, DataSource, flow) {
+define(['app/module/ModuleBase', "js/data/DataSource", "sprd/model/Product"], function(ModuleBase, DataSource, Product) {
     return ModuleBase.inherit('app.module.CreateClass', {
 
         defaults: {
             designs: "{user.designs}",
 
             // the product
-            product: null
-
+            product: Product,
+            currentView: null
         },
 
         inject: {
@@ -20,7 +20,41 @@ define(['app/module/ModuleBase', "js/data/DataSource", "flow"], function(ModuleB
                 });
             }
 
-        }.onChange('designs')
+        }.onChange('designs'),
+
+        initialize: function () {
+            this.bind('product', 'change:productType', this._onProductTypeChange, this);
+
+            this.callBase();
+        },
+
+        _onProductTypeChange: function (e) {
+            if (e && e.$) {
+                var productType = e.$;
+                productType.fetch();
+            }
+        },
+
+        start: function() {
+            var user = this.$.user,
+                product = this.$.product;
+
+            if (!(product && product.$.productType)) {
+                user.$.productTypes.fetchPage(0, {
+                    fullData: true
+                }, function() {
+                    var productType = user.get('productTypes[1]');
+
+                    product.set('productType', productType);
+                    if (productType) {
+                        product.set('appearance', productType.getDefaultAppearance());
+                    }
+                });
+            }
+
+            this.callBase();
+
+        }
 
     });
 });
