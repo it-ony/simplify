4dac545c77835bec6ea4e9c24e97e7944f316fd0
Tony Findeisen <github@it-ony.de>
2012-12-18 11:46:35 +0100

    setup create2Module for prototyping

---
+++ public/app/module/Create2ModuleClass.js
@@ -0,0 +1,59 @@
+define(['app/module/ModuleBase', "js/data/DataSource", "sprd/model/Product", "flow"], function(ModuleBase, DataSource, Product, flow) {
+    return ModuleBase.inherit('app.module.CreateClass', {
+
+        defaults: {
+            // the product
+            product: null
+        },
+
+        inject: {
+            api: DataSource
+        },
+
+        _onProductTypeChange: function (e) {
+            if (e && e.$) {
+                var productType = e.$;
+                productType.fetch();
+            }
+        },
+
+        start: function(parameter, callback) {
+            var self = this,
+                user = this.$.user,
+                product = user.getCollection('products').createItem(),
+                productType;
+
+            this.set('product', product);
+
+            flow()
+                .seq(function(cb) {
+                    if (!(product && product.$.productType)) {
+                        user.$.productTypes.fetchPage(0, null, function () {
+                            productType = user.get('productTypes[1]');
+                            product.set('productType', productType);
+                            cb();
+                        });
+                    } else {
+                        cb();
+                    }
+                })
+                .seq(function(cb) {
+                    if (productType) {
+                        productType.fetch(null, cb);
+                    } else {
+                        cb();
+                    }
+                })
+                .seq(function(){
+                    if (productType) {
+                        product.set('appearance', productType.getDefaultAppearance());
+                    }
+                })
+                .exec(function() {
+                    self.start.baseImplementation.call(self, parameter, callback);
+                });
+
+        }
+
+    });
+});
